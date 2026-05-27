cf71cbf30794be79447b56d957ab6d813def4722
Chef <family@kitchen.local>
2026-05-27 22:26:40 +0800

    fix: switch to HashRouter for GitHub Pages, add base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import { CartProvider } from './context/CartContext'
 import Layout from './components/Layout'
@@ -30,12 +30,12 @@ function AppRoutes() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <CartProvider>
           <AppRoutes />
         </CartProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
